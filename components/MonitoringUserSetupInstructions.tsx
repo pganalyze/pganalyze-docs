d2119c74239e8fdac3a652ae9a13f706471b8c76
Maciek Sakrejda <m.sakrejda@gmail.com>
2021-01-12 14:16:03 -0800

    Drop 9.3 instructions
    Postgres 9.3 has been out of support upstream for 2+ years.

--- components/MonitoringUserSetupInstructions.tsx
+++ components/MonitoringUserSetupInstructions.tsx
@@ -4,7 +4,6 @@ import TabPanel, { TabItem } from './TabPanel'
 import MonitoringUser10 from "../install/_monitoring_user_10.mdx";
 import MonitoringUser96 from "../install/_monitoring_user_96.mdx";
 import MonitoringUser94 from "../install/_monitoring_user_94.mdx";
-import MonitoringUserOld from "../install/_monitoring_user_old.mdx";
 
 type Props = {
   minPostgres: number;
@@ -18,7 +17,6 @@ const MonitoringUserSetupInstructions: React.FunctionComponent<Props> = ({
     [ 'monitoring_user_10', 100000, 'PostgreSQL 10+', MonitoringUser10 ],
     [ 'monitoring_user_96', 90600, 'PostgreSQL 9.6', MonitoringUser96 ],
     [ 'monitoring_user_94', 90500, 'PostgreSQL 9.4 and 9.5', MonitoringUser94 ],
-    [ 'monitoring_user_old', 90300, 'PostgreSQL 9.3 and older', MonitoringUserOld ],
   ].filter(([_id, version, _label ]) => minPostgres <= version) as SetupOpt[];
 
   const tabs = opts.map<TabItem>(opt => [opt[0], opt[2]])
