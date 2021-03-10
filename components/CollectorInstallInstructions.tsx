5f30059c4bb34c0a822b8b36cd51b9a99bde287c
Maciek Sakrejda <m.sakrejda@gmail.com>
2021-03-09 17:00:15 -0800

    Document collector install script (#38)
    Instead of manual package install instructions, prompt users to download and run
    the install script we're adding to the collector to autodetect the platform and streamline
    package installation.
    
    See https://github.com/pganalyze/collector/pull/158

---
+++ components/CollectorInstallInstructions.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useCodeBlock } from './CodeBlock'
+
+import RepositorySigningKey from './RepositorySigningKey'
+
+type Props = {
+  apiKey?: string
+  guided?: boolean
+}
+
+const CollectorInstallInstructions: React.FunctionComponent<Props> = ({apiKey, guided}) => {
+  const env = {};
+  if (apiKey) {
+    env['PGA_API_KEY'] = apiKey
+  }
+  if (guided) {
+    env['PGA_GUIDED_SETUP'] = 'true'
+  }
+
+  return <CollectorEnvInstallInstructions env={env} />
+}
+
+const CollectorEnvInstallInstructions: React.FunctionComponent<{
+  env: { [key: string]: string }
+}> = ({env}) => {
+  const CodeBlock = useCodeBlock();
+  let bashCmd: string;
+  if (Object.keys(env).length === 0) {
+    bashCmd = 'bash';
+  } else {
+    bashCmd = Object.entries(env).reduce((cmdStr, [ nextKey, nextVal ]) => {
+      return cmdStr + ` ${nextKey}=${nextVal}`
+    }, 'env') + ' bash'
+  }
+  return (
+    <>
+      <p>
+        We recommend running our install script to automatically detect your platform and
+        install the correct package:
+      </p>
+      <CodeBlock>
+        curl https://packages.pganalyze.com/collector-install.sh | {bashCmd}
+      </CodeBlock>
+      <RepositorySigningKey small />
+      <p>
+        Alternately, you can follow the <a href="https://pganalyze.com/docs/collector/packages">manual install instructions</a>.
+      </p>
+    </>
+  )
+}
+
+export default CollectorInstallInstructions;
--- components/RepositorySigningKey.tsx
+++ components/RepositorySigningKey.tsx
@@ -3,11 +3,35 @@ import { useIcon } from './WithIcons';
 
 import styles from './style.module.scss';
 
-const RepositorySigningKey: React.FunctionComponent = () => {
-  const SecureIcon = useIcon('secure');
-  const keyUrl = 'https://packages.pganalyze.com/pganalyze_signing_key.asc';
+type Props = {
+  small?: boolean
+}
+
+const KEY_URL = 'https://packages.pganalyze.com/pganalyze_signing_key.asc';
+
+const RepositorySigningKey: React.FunctionComponent<Props> = ({small}) => {
   return (
     <div style={{fontSize: '14px', padding: '6px 16px 4px', borderTop: '1px solid gray', borderBottom: '1px solid gray', marginBottom: '16px'}}>
+      {small ? <SmallKey /> : <Key />}
+    </div>
+  )
+}
+
+const SmallKey: React.FunctionComponent = () => {
+  const SecureIcon = useIcon('secure');
+  return (
+    <div>
+      <strong>
+        <SecureIcon className={styles.secureIcon} />Repository Signing Key Fingerprint
+      </strong>: C09B 2CAB 0DB3 78F6 E7FD 93F1 0E6D EC71 A2B5 F2F9
+    </div>
+  )
+}
+
+const Key: React.FunctionComponent = () => {
+  const SecureIcon = useIcon('secure');
+  return (
+    <>
       <div>
         <strong>
           <SecureIcon className={styles.secureIcon} />Repository Signing Key
@@ -17,9 +41,9 @@ const RepositorySigningKey: React.FunctionComponent = () => {
         <strong>Fingerprint</strong>: C09B 2CAB 0DB3 78F6 E7FD 93F1 0E6D EC71 A2B5 F2F9 
       </div>
       <div>
-        <strong>Download</strong>: <a href={keyUrl}>{keyUrl}</a>
+        <strong>Download</strong>: <a href={KEY_URL}>{KEY_URL}</a>
       </div>
-    </div>
+    </>
   )
 }
 
