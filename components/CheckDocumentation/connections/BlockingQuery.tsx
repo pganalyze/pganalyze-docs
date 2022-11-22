586d21b52e28c702c4dd39a11926d876b10de824
Keiko Oda <keiko713@gmail.com>
2022-11-22 13:06:39 +0900

    Apply reviews

--- components/CheckDocumentation/connections/BlockingQuery.tsx
+++ components/CheckDocumentation/connections/BlockingQuery.tsx
@@ -16,14 +16,15 @@ const BlockingQueryTrigger: React.FunctionComponent<CheckTriggerProps> = ({
   return (
     <>
       <p>
-        Detects queries currently blocking more than the specified threshold of{" "}
-        <code>{config.settings["blocked_count"]}</code> queries. When the oldest
-        blocked query is running for longer than the specified threshold of{" "}
-        <code>{config.settings["warning_blocked_age_secs"]}</code> seconds,
-        creates an issue with severity "warning". Escalates to "critical" if the
-        oldest blocked query continues to run longer than{" "}
+        Detects queries currently blocking equal or more than the specified
+        threshold of <code>{config.settings["blocked_count"]}</code> queries.
+        When any blocked query is running for longer than the specified
+        threshold of <code>{config.settings["warning_blocked_age_secs"]}</code>{" "}
+        seconds, creates an issue with severity "warning". Escalates to
+        "critical" if any blocked query continues to run longer than{" "}
         <code>{config.settings["critical_blocked_age_secs"]}</code> seconds.
-        Resolves automatically once these queries stop meeting the criteria.
+        Resolves automatically once these queries complete and release their
+        locks.
       </p>
       <p>
         Ignores any blocking queries that contain the{" "}
@@ -40,17 +41,16 @@ const BlockingQueryGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
 }) => {
   const Link = useSmartAnchor();
   const issueDetails = issue && JSON.parse(issue.detailsJson);
-  const showAutovacuum = issueDetails?.["has_autovacuum_to_prevent_wraparound"];
+  const showAutovacuum =
+    !issueDetails || issueDetails["has_autovacuum_to_prevent_wraparound"];
   return (
     <div>
       <h4>Impact</h4>
       <p>
-        Queries that are blocking many other queries for certain amount of time
-        likely holding some strong locks. These queries can block some trivial
-        SELECT queries sometimes, which can cause the whole system down. They
-        can also prevent VACUUM (including autovacuum) from cleaning up dead
-        rows, leading to table or index bloat. They may also be consuming a lot
-        of I/O or CPU resources, impacting overall performance.
+        Queries that hold contended locks for a significant period of time may
+        end up preventing other queries from making progress. This can even
+        block simple SELECT queries, and in some cases, it can affect overall
+        system performance or it can cause the system to run out of connections.
       </p>
       <h4>Common Causes</h4>
       <ul>
@@ -58,36 +58,15 @@ const BlockingQueryGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
           <li>
             <h5>Autovacuum (to prevent wraparound)</h5>
             <p>
-              In general, autovacuum workers don't block other queries. Even if
-              the other query tries to acquire a lock that conflicts with the
-              running autovacuum, lock acquisition will interrupt the
-              autovacuum. However, if the autovacuum is running to prevent
-              transaction ID wraparound, the vacuum won't be interrupted and
-              will block other queries trying to acquire conflicting locks.
+              In general, autovacuum won't block other queries. If a query tries
+              to acquire a lock that conflicts with a running autovacuum,
+              autovacuum will be interrupted so that the query can proceed.
+              However, if autovacuum is running to prevent transaction ID
+              wraparound, it will not yield like this and will block other
+              queries trying to acquire conflicting locks.
             </p>
           </li>
         )}
-        <li>
-          <h5>Locks</h5>
-          <p>
-            The queries may be holding some strong locks. Check the{" "}
-            <Link to={backendsUrl}>Connection Traces</Link> page for the queries
-            these queries are blocking. You can check{" "}
-            <strong>Wait Events</strong> page for the queries waiting for these
-            blocking queries, and look for any wait events of type{" "}
-            <strong>Lock</strong>. If you have{" "}
-            <SettingLink setting="log_lock_waits" /> turned on, you can also see
-            details about locking in the <strong>Logs</strong> page for these
-            queries.
-          </p>
-          <p>
-            <code>ACCESS EXCLUSIVE</code> lock xis the strongest lock that can
-            even block a <code>SELECT</code> query. This lock is acquired by the
-            commands like <code>VACUUM FULL</code>. One of the table-rewriting
-            variants of <code>ALTER TABLE</code> also acquire a lock at this
-            level.
-          </p>
-        </li>
         <li>
           <h5>Long-running migration</h5>
           <p>
@@ -100,19 +79,56 @@ const BlockingQueryGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
             avoid table rewrites.
           </p>
         </li>
+        <li>
+          <h5>Other long-running queries</h5>
+          <p>
+            A long running query could cause many blocking queries when that
+            query is holding locks that conflict with many other queries.
+            Navigate to the <Link to={backendsUrl}>Connection Traces</Link> page
+            to check the overview of which query is blocking which queries to
+            understand the impact. It is useful to know what kind of locks the
+            blocking query is holding and what kind of locks the blocked queries
+            are waiting for. You can check the wait event of these locks in the{" "}
+            <strong>Wait Events</strong> page, with any wait events of type{" "}
+            <strong>Lock</strong>. If you have{" "}
+            <SettingLink setting="log_lock_waits" /> turned on, you can also see
+            details about locking in the <strong>Logs</strong> page for these
+            queries.
+          </p>
+          <p>
+            You can run the queries like{" "}
+            <SQL
+              inline
+              sql={`SELECT * FROM pg_locks WHERE pid = '<blocking_pid>';`}
+            />{" "}
+            to obtain more detailed lock information of the blocking query to
+            see which locks the blocking query is holding. You can also run this
+            query with the blocked query like{" "}
+            <SQL
+              inline
+              sql={`SELECT * FROM pg_locks WHERE pid = '<blocked_pid>' WHERE NOT granted;`}
+            />{" "}
+            to see which locks it's waiting for. These information can be used
+            for your investigation to understand why this has happened and how
+            to change the query to prevent this from happening.
+          </p>
+        </li>
       </ul>
       <h4>Solution</h4>
       <p>
         You can cancel these queries with{" "}
         <SQL inline sql={`SELECT pg_cancel_backend('<query_pid>');`} />. Note
         that this only treats the symptom: if the query runs again without any
-        changes, it is likely to cause the blocking.
+        changes, it is likely to acquire the same locks and block other queries
+        again.
       </p>
       {showAutovacuum && (
         <p>
-          If you're canceling the autovacuum, please note that the autovacuum to
-          prevent wraparound is really important to run and it will most likely
-          come back.
+          If you're canceling an autovacuum to prevent wraparound, note that it
+          does critical work. It will restart and need to be completed
+          eventually, acquiring the same locks. Consider tuning your vacuum
+          settings to be more aggressive to avoid the need for explicit vacuums
+          to prevent wraparound.
         </p>
       )}
     </div>
@@ -121,7 +137,7 @@ const BlockingQueryGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
 
 const documentation: CheckDocs = {
   description:
-    "<p><b>(Available on Early Access Only)</b> Alerts on queries currently blocking more than the specified threshold queries for longer than the specified threshold time. This check only triggers on queries that are currently meeting both criteria and auto-resolves once the queries stop meeting the criteria.</p><p>Ignores any blocking queries that contain the <code>/* pganalyze:no-alert */</code> or <code>/* pganalyze=no-alert */</code> magic comment.</p>",
+    "<p><b>(Available on Early Access Only)</b> Alerts on queries currently blocking equal or more than the specified threshold queries for longer than the specified threshold time. This check only triggers on queries that are currently meeting both criteria and auto-resolves once the queries stop meeting the criteria.</p><p>Ignores any blocking queries that contain the <code>/* pganalyze:no-alert */</code> or <code>/* pganalyze=no-alert */</code> magic comment.</p>",
   Trigger: BlockingQueryTrigger,
   Guidance: BlockingQueryGuidance,
 };
