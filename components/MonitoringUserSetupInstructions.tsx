e9ff562082b3d05551c9f0537515fa07a6edd7a9
Maciek Sakrejda <m.sakrejda@gmail.com>
2023-02-08 10:27:26 -0800

    Drop documentation specific to Postgres 9.5 and older
    These are no longer supported.

--- components/MonitoringUserSetupInstructions.tsx
+++ components/MonitoringUserSetupInstructions.tsx
@@ -11,7 +11,7 @@ type Props = {
 };
 
 const MonitoringUserSetupInstructions: React.FunctionComponent<Props> = ({
-  minPostgres = 90200,
+  minPostgres = 90600,
   password = 'mypassword',
   adminUsername,
   noPgMonitor
@@ -25,7 +25,6 @@ const MonitoringUserSetupInstructions: React.FunctionComponent<Props> = ({
   const opts: SetupOpt[] = [
     [ 'monitoring_user_10', 100000, 'PostgreSQL 10+', MonitoringUser10 ],
     [ 'monitoring_user_96', 90600, 'PostgreSQL 9.6', MonitoringUser96 ],
-    [ 'monitoring_user_94', 90500, 'PostgreSQL 9.4 and 9.5', MonitoringUser94 ],
   ].filter(([_id, version, _label ]) => minPostgres <= version) as SetupOpt[];
 
   const tabs = opts.map<TabItem>(opt => [opt[0], opt[2]])
@@ -222,54 +221,6 @@ $$ LANGUAGE sql VOLATILE SECURITY DEFINER;`}</CodeBlock>
       </p>
       <CodeBlock>
       {`CREATE OR REPLACE FUNCTION pganalyze.reset_stat_statements() RETURNS SETOF void AS
-$$
-  /* pganalyze-collector */ SELECT * FROM public.pg_stat_statements_reset();
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;`}
-      </CodeBlock>
-    </>
-  )
-}
-
-const MonitoringUser94: React.FunctionComponent<{password: string}> = ({password}) => {
-  const CodeBlock = useCodeBlock();
-  return (
-    <>
-      <CodeBlock>
-        {`CREATE USER pganalyze WITH PASSWORD '${password}' CONNECTION LIMIT 5;
-
-CREATE SCHEMA pganalyze;
-GRANT USAGE ON SCHEMA pganalyze TO pganalyze;
-GRANT USAGE ON SCHEMA public TO pganalyze;
-
-CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_statements(showtext boolean = true) RETURNS SETOF pg_stat_statements AS
-$$
-  /* pganalyze-collector */ SELECT * FROM public.pg_stat_statements(showtext);
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_activity() RETURNS SETOF pg_stat_activity AS
-$$
-  /* pganalyze-collector */ SELECT * FROM pg_catalog.pg_stat_activity;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_column_stats() RETURNS SETOF pg_stats AS
-$$
-  /* pganalyze-collector */ SELECT schemaname, tablename, attname, inherited, null_frac, avg_width,
-  n_distinct, NULL::anyarray, most_common_freqs, NULL::anyarray, correlation, NULL::anyarray,
-  most_common_elem_freqs, elem_count_histogram
-  FROM pg_catalog.pg_stats;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_replication() RETURNS SETOF pg_stat_replication AS
-$$
-  /* pganalyze-collector */ SELECT * FROM pg_catalog.pg_stat_replication;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;`}</CodeBlock>
-      <p>
-        If you enabled the optional reset mode (usually not required), you will also need this helper method:
-      </p>
-      <CodeBlock>
-        {`CREATE OR REPLACE FUNCTION pganalyze.reset_stat_statements() RETURNS SETOF void AS
 $$
   /* pganalyze-collector */ SELECT * FROM public.pg_stat_statements_reset();
 $$ LANGUAGE sql VOLATILE SECURITY DEFINER;`}
