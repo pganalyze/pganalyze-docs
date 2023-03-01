81c25a6c5b377c483d310c1964ebe58f1d771d53
Maciek Sakrejda <m.sakrejda@gmail.com>
2023-03-01 10:25:33 -0800

    Update monitoring user setup instructions
     - drop 9.6 instructions (since we've dropped 9.6 support)
     - drop the minPostgres flag to the MonitoringUserSetupInstructions
       component since instructions are now uniform
     - use the pre-pg_monitor instructions for pg_stat_statements_reset
       on current versions, since this is not actually included in
       pg_monitor permissions

--- components/MonitoringUserSetupInstructions.tsx
+++ components/MonitoringUserSetupInstructions.tsx
@@ -1,41 +1,21 @@
 import React from "react";
 
 import { useCodeBlock } from "./CodeBlock";
-import TabPanel, { TabItem } from './TabPanel'
 
 type Props = {
-  minPostgres: number;
   password: string;
   adminUsername?: string;
   noPgMonitor?: boolean;
 };
 
 const MonitoringUserSetupInstructions: React.FunctionComponent<Props> = ({
-  minPostgres = 90600,
   password = 'mypassword',
   adminUsername,
   noPgMonitor
 }) => {
-  type SetupOpt = [
-    id: string,
-    version: number,
-    label: string,
-    instructions: React.ComponentType<{password: string, noPgMonitor?: boolean, adminUsername?: string}>
-  ];
-  const opts: SetupOpt[] = [
-    [ 'monitoring_user_10', 100000, 'PostgreSQL 10+', MonitoringUser10 ],
-    [ 'monitoring_user_96', 90600, 'PostgreSQL 9.6', MonitoringUser96 ],
-  ].filter(([_id, version, _label ]) => minPostgres <= version) as SetupOpt[];
-
-  const tabs = opts.map<TabItem>(opt => [opt[0], opt[2]])
   return (
     <>
-      <TabPanel items={tabs}>
-        {(idx: number) => {
-          const ActiveTab = opts[idx][3];
-          return <ActiveTab password={password} noPgMonitor={noPgMonitor} />
-        }}
-      </TabPanel>
+      <MonitoringUserBase password={password} noPgMonitor={noPgMonitor} />
       <MonitoringUserColumnStats username="pganalyze" adminUsername={adminUsername} />
     </>
   );
@@ -153,7 +133,7 @@ $$ LANGUAGE plpgsql VOLATILE SECURITY DEFINER;`}
   );
 }
 
-const MonitoringUser10: React.FunctionComponent<{
+const MonitoringUserBase: React.FunctionComponent<{
   password: string;
   noPgMonitor?: boolean;
 }> = ({password, noPgMonitor}) => {
@@ -172,52 +152,8 @@ $$
   /* pganalyze-collector */ SELECT * FROM pg_catalog.pg_stat_replication;
 $$ LANGUAGE sql VOLATILE SECURITY DEFINER;`}
       </CodeBlock>
-    </>
-  )
-}
-
-const MonitoringUser96: React.FunctionComponent<{password: string}> = ({password}) => {
-  const CodeBlock = useCodeBlock();
-  return (
-    <>
-      <CodeBlock>
-        {`CREATE USER pganalyze WITH PASSWORD '${password}' CONNECTION LIMIT 5;
-
-CREATE SCHEMA pganalyze;
-GRANT USAGE ON SCHEMA pganalyze TO pganalyze;
-GRANT USAGE ON SCHEMA public TO pganalyze;
-
-CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_statements(showtext boolean = true) RETURNS SETOF pg_stat_statements AS
-$$
-  /* pganalyze-collector */ SELECT * FROM public.pg_stat_statements(showtext);
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_activity() RETURNS SETOF pg_stat_activity AS
-$$
-  /* pganalyze-collector */ SELECT * FROM pg_catalog.pg_stat_activity;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_column_stats() RETURNS SETOF pg_stats AS
-$$
-  /* pganalyze-collector */ SELECT schemaname, tablename, attname, inherited, null_frac, avg_width,
-  n_distinct, NULL::anyarray, most_common_freqs, NULL::anyarray, correlation, NULL::anyarray,
-  most_common_elem_freqs, elem_count_histogram
-  FROM pg_catalog.pg_stats;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_replication() RETURNS SETOF pg_stat_replication AS
-$$
-  /* pganalyze-collector */ SELECT * FROM pg_catalog.pg_stat_replication;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;
-
-CREATE OR REPLACE FUNCTION pganalyze.get_stat_progress_vacuum() RETURNS SETOF pg_stat_progress_vacuum AS
-$$
-  /* pganalyze-collector */ SELECT * FROM pg_catalog.pg_stat_progress_vacuum;
-$$ LANGUAGE sql VOLATILE SECURITY DEFINER;`}</CodeBlock>
       <p>
-        If you enabled the optional reset mode (usually not required), you will also need this helper method:
+        If you enable the optional reset mode (usually not required), you will also need this helper method:
       </p>
       <CodeBlock>
       {`CREATE OR REPLACE FUNCTION pganalyze.reset_stat_statements() RETURNS SETOF void AS
