dcd456360bfa78488578181fe44704e39e9af4bd
Keiko Oda <keiko713@gmail.com>
2023-03-29 09:29:01 +0900

    More fix of not returning guidance

--- components/CheckDocumentation/vacuum/OptimizeTableBloat.tsx
+++ components/CheckDocumentation/vacuum/OptimizeTableBloat.tsx
@@ -8,7 +8,7 @@ import {
 import { formatBytes } from "../../../util/format";
 
 const OptimizeTableBloatTrigger: React.FunctionComponent<CheckTriggerProps> = ({
-  config
+  config,
 }) => {
   return (
     <p>
@@ -28,41 +28,44 @@ const OptimizeTableBloatTrigger: React.FunctionComponent<CheckTriggerProps> = ({
 const OptimizeTableBloatGuidance: React.FunctionComponent<
   CheckGuidanceProps
 > = () => {
-  <div>
-    <h4>Impact</h4>
-    <p>
-      Table bloat refers to the phenomenon where a table's physical size on disk
-      is larger than its actual data size. In Postgres, any DELETEs or UPDATEs
-      will create dead rows.
-      <code>VACUUM</code> command and autovacuum removes dead rows and make such
-      space reusable for future INSERTs and UPDATEs. However, if VACUUM is not
-      happening in the right timing, dead rows will be kept as table bloat and
-      table bloat can keep growing. Table bloat is problematic for several
-      reasons:
-    </p>
-    <ul>
-      <li>
-        <h5>Slower queries</h5>
-        <p>
-          When a table is bloated, it takes more time to read and write data
-          from the disk, which can slow down queries that access the table.
-        </p>
-      </li>
-      <li>
-        <h5>Increased disk usage</h5>
-        <p>
-          Table bloat can cause the disk space usage to increase unnecessarily,
-          which can lead to disk space shortages and degraded performance.
-        </p>
-      </li>
-    </ul>
-    <h4>Solution</h4>
-    <p>
-      You can adjust the timing of autovacuum by changing the config variables
-      related to autovacuum. Use VACUUM simulator to adjust the config variable
-      to find a good spot for the table and the server.
-    </p>
-  </div>;
+  return (
+    <div>
+      <h4>Impact</h4>
+      <p>
+        Table bloat refers to the phenomenon where a table's physical size on
+        disk is larger than its actual data size. In Postgres, any DELETEs or
+        UPDATEs will create dead rows.
+        <code>VACUUM</code> command and autovacuum removes dead rows and make
+        such space reusable for future INSERTs and UPDATEs. However, if VACUUM
+        is not happening in the right timing, dead rows will be kept as table
+        bloat and table bloat can keep growing. Table bloat is problematic for
+        several reasons:
+      </p>
+      <ul>
+        <li>
+          <h5>Slower queries</h5>
+          <p>
+            When a table is bloated, it takes more time to read and write data
+            from the disk, which can slow down queries that access the table.
+          </p>
+        </li>
+        <li>
+          <h5>Increased disk usage</h5>
+          <p>
+            Table bloat can cause the disk space usage to increase
+            unnecessarily, which can lead to disk space shortages and degraded
+            performance.
+          </p>
+        </li>
+      </ul>
+      <h4>Solution</h4>
+      <p>
+        You can adjust the timing of autovacuum by changing the config variables
+        related to autovacuum. Use VACUUM simulator to adjust the config
+        variable to find a good spot for the table and the server.
+      </p>
+    </div>
+  );
 };
 
 const documentation: CheckDocs = {
