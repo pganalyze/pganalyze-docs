8878612451c9311ff9430cafccc80a73bffbb1f7
Keiko Oda <keiko713@gmail.com>
2023-06-23 21:05:57 +0900

    Reword trigger to fit to a new implementation

--- components/CheckDocumentation/vacuum/UnnecessaryTableGrowth.tsx
+++ components/CheckDocumentation/vacuum/UnnecessaryTableGrowth.tsx
@@ -14,12 +14,12 @@ const UnnecessaryTableGrowthTrigger: React.FunctionComponent<
     <p>
       Detects when a table has experienced unnecessary growth due to
       insufficient VACUUM. It analyzes the table statistic from the past seven
-      days and calculates the amount of growth that could have been avoided if
-      VACUUM had been performed more frequently. Creates an issue when the
-      unnecessary growth exceeds{" "}
-      <code>{formatBytes(Number(config.settings["notify_bytes"]))}</code>, as
-      well as <code>{config.settings["notify_pct"]}%</code> of the current size.
-      Resolves once the unnecessary growth decreases.
+      days and calculates the amount of rows that could have been avoided to
+      grow if VACUUM had been performed more frequently. Creates an issue when
+      such rows exceeds <code>{config.settings["notify_pct"]}%</code> of total
+      new rows, as well as{" "}
+      <code>{formatBytes(Number(config.settings["notify_bytes"]))}</code>{" "}
+      accumulated. Resolves once the unnecessary growth decreases.
     </p>
   );
 };
