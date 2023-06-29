5736953e07971a470754d59ee9bf423f9ed60db9
Keiko Oda <keiko713@gmail.com>
2023-06-29 17:48:47 +0900

    Show the link to Index Advisor doc from MissingIndexGuidance

--- components/CheckDocumentation/index_advisor/MissingIndex.tsx
+++ components/CheckDocumentation/index_advisor/MissingIndex.tsx
@@ -1,26 +1,43 @@
 import React from "react";
 
-import { CheckDocs, CheckGuidanceProps, CheckTriggerProps } from "../../../util/checks";
+import {
+  CheckDocs,
+  CheckGuidanceProps,
+  CheckTriggerProps,
+} from "../../../util/checks";
 
 const MissingIndexTrigger: React.FunctionComponent<CheckTriggerProps> = () => {
   return (
     <p>
-      Detects when a table might be missing an index based on the query workload over the last
-      seven days and creates an issue with severity "info". Resolves once the recommended index
-      has been created, or the workload has changed such that the index is no longer relevant.
+      Detects when a table might be missing an index based on the query workload
+      over the last seven days and creates an issue with severity "info".
+      Resolves once the recommended index has been created, or the workload has
+      changed such that the index is no longer relevant.
     </p>
   );
 };
 
-const MissingIndexGuidance: React.FunctionComponent<CheckGuidanceProps> = () => {
-  // TODO: the Missing Index check has dynamic guidance, so this component is not used in-app,
-  // but we still need something for the public documentation (possibly linking to other Index
-  // Advisor docs).
-  return null;
+const MissingIndexGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
+  issue,
+}) => {
+  if (issue) {
+    // The Missing Index check has dynamic guidance, so this component is not used in-app (aka when `issue` is present)
+    return null;
+  }
+  return (
+    <p>
+      You can learn about Index Advisor and missing index opportunities in{" "}
+      <a href="https://pganalyze.com/docs/index-advisor/reason-about-opportunities">
+        Index Advisor documentation
+      </a>
+      .
+    </p>
+  );
 };
 
 const documentation: CheckDocs = {
-  description: "Detects when a table might be missing an index based on the query workload.",
+  description:
+    "Detects when a table might be missing an index based on the query workload.",
   Trigger: MissingIndexTrigger,
   Guidance: MissingIndexGuidance,
 };
