cec34da4e56f95c7bc4ec995b4f99b4470915c61
Keiko Oda <keiko713@gmail.com>
2023-07-18 19:06:56 +0900

    Update docs to focus on what actually happens, not about txid wraparound

--- components/CheckDocumentation/vacuum/TxidWraparound.tsx
+++ components/CheckDocumentation/vacuum/TxidWraparound.tsx
@@ -30,31 +30,43 @@ const TxidWraparoundGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
   return (
     <div>
       <h4>Impact</h4>
-      <p>The transaction ID is close to wraparound.</p>
       <p>
-        When the transaction ID wraparound happens, Postgres is unable to start
-        a new transaction and essentially makes the database down. Postgres
-        usually runs anti-wraparound autovacuums to prevent this from happening,
-        but if such VACUUMs are either not running or are running but unable to
-        freeze rows for some reasons, the transaction age (and the utilization)
-        keeps growing and eventually hit the point of wraparound. With Postgres
-        14+, there is a special type of failsafe VACUUM which takes
-        extraordinary measures to avoid system-wide transaction ID wraparound
-        failure. It is recommended to take any actions to prevent the
-        wraparound, or even failsafe from happening.
+        The transaction ID usage is high. Postgres usually runs anti-wraparound
+        autovacuums to keep the usage low by freezing old rows. However, if such
+        VACUUMs are either not running or are running but unable to freeze rows
+        for some reasons, the transaction usage keeps growing.
+      </p>
+      <p>
+        The anti-wraparound VACUUM needs to happen at some point and this is not
+        avoidable. If VACUUM is somehow not run in timely manner, the upcoming
+        VACUUM becomes more and more expensive, potentially causes the overall
+        performance degradation or simply takes really long time to finish. The
+        anti-wraparound VACUUM holds <code>SHARE UPDATE EXCLUSIVE</code> lock,
+        which can block DDL statements.
+      </p>
+      <p>
+        When the transaction ID usage reaches to 99.85% (3M transactions left),
+        the system will shut down and refuse to start any new transactions. This
+        is to prevent any data corruption from happening by running out
+        transaction ID.
+      </p>
+      <p>
+        With Postgres 14+, there is a special type of failsafe VACUUM which
+        takes extraordinary measures to avoid the shutdown. While this is very
+        useful, you still want to avoid even this from happening, to avoid any
+        impact by this VACUUM.
       </p>
       <h4>Common Cases</h4>
       <ul>
         <li>
           <h5>Ineffectual autovacuum settings</h5>
           <p>
-            Autovacuums, especially anti-wraparound autovacuums are meant to
-            prevent the wraparound from happening. When autovacuum is turned
-            off, or the setup of anti-wraparound autovacuum is not fitting well
-            for the database usage, it is possible that autovacuum is not
-            working as intended and causing the utilization growth. Make sure
-            that autovacuum is turned on, and revisit the setting to ensure that
-            anti-wraparound autovacuums will run periodically.
+            Autovacuums, especially anti-wraparound autovacuums, are meant to
+            prevent transaction ID wraparound by freezing old rows and keeping
+            transaction ID usage low. When <code>vacuum_freeze_min_age</code> is
+            set too high, anti-wraparound autovacuum will not be triggered in
+            timely manner and cause the usage growth. Revisit the setting to
+            ensure that anti-wraparound autovacuums will run periodically.
           </p>
         </li>
         <li>
@@ -66,8 +78,8 @@ const TxidWraparoundGuidance: React.FunctionComponent<CheckGuidanceProps> = ({
             blocker is a long running transaction, or a transaction holding some
             locks that could cancel autovacuums. It is important to make sure
             that there is no such transactions so that VACUUMs can freeze rows
-            well. Check for any long-running transactions on the{" "}
-            <Link to={backendsUrl}>Connections</Link> page.
+            well. Check for any long-running transactions or its lock state on
+            the <Link to={backendsUrl}>Connections</Link> page.
           </p>
         </li>
       </ul>
