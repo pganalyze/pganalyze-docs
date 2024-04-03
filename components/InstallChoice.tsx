bdd2a5f1b732ce79ba94dfe84c20da4fa4fbb782
Keiko Oda <keiko713@gmail.com>
2024-04-03 14:10:40 +0900

    Update install choice buttons (#233)
    Apply a new design for install choice (of providers) buttons. Also update logos to use the latest, as well as svg.

---
+++ components/InstallChoice.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import styles from "../style.module.scss";
+
+type InstallChoiceItem = {
+  link: string;
+  img: string;
+  providerName: string;
+  text: string;
+};
+
+type Props = {
+  docsRoot: boolean;
+  items: InstallChoiceItem[];
+};
+
+const InstallChoice: React.FunctionComponent<Props> = ({ docsRoot, items }) => {
+  return (
+    <>
+      <div className={styles.overviewInstallChoice}>
+        {items.map((item) => {
+          const linkTo = `${docsRoot ? "/docs/" : ""}${item.link}`;
+          const imgAlt = `Logo of ${item.providerName}`;
+          return (
+            <a
+              className={styles.overviewInstallChoiceStep}
+              href={linkTo}
+              key={item.providerName}
+            >
+              <div className={styles.overviewInstallChoiceProvider}>
+                <img src={item.img} alt={imgAlt} />
+                {item.providerName}
+              </div>
+              <div className={styles.overviewInstallChoiceProviderText}>
+                {item.text}
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default InstallChoice;
