1219f50b843c1fc82933700f44aa77786d49e2f4
Keiko Oda <keiko713@gmail.com>
2024-07-15 17:45:31 +0900

    Update collector test to remove --reload

--- components/CollectorLogTest.tsx
+++ components/CollectorLogTest.tsx
@@ -17,7 +17,7 @@ I Successfully reloaded pganalyze collector (PID 123)`
           and apply the collector configuration (for container environments, simply
           restart the collector):
         </p>
-        <CodeBlock>sudo pganalyze-collector --test --reload</CodeBlock>
+        <CodeBlock>sudo pganalyze-collector --test</CodeBlock>
         <CodeBlock>{sampleOutput}</CodeBlock>
       </>
     )
@@ -34,7 +34,7 @@ I Successfully reloaded pganalyze collector (PID 123)`
     return (
       <>
         <p>You can now test and apply the collector configuration:</p>
-        <CodeBlock>sudo pganalyze-collector --test --reload</CodeBlock>
+        <CodeBlock>sudo pganalyze-collector --test</CodeBlock>
         <CodeBlock>{sampleOutput}</CodeBlock>
         <p>
           If you are getting an error, it sometimes helps to run the
