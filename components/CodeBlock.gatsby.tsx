090f74effa596af055415354720cb5d509600720
Ryan Booz <ryan@pganalyze.com>
2025-07-18 11:37:31 -0400

    Impliment code copy blocks across documentation pages

--- components/CodeBlock.gatsby.tsx
+++ components/CodeBlock.gatsby.tsx
@@ -3,6 +3,10 @@ import CopyToClipboard from "./CopyToClipboard";
 import hljs from "highlight.js/lib/core";
 import json from "highlight.js/lib/languages/json";
 import sql from "highlight.js/lib/languages/pgsql";
+import yaml from "highlight.js/lib/languages/yaml";
+import bash from "highlight.js/lib/languages/bash";
+import ruby from "highlight.js/lib/languages/ruby";
+import python from "highlight.js/lib/languages/python";
 import styles from "./style.module.scss";
 
 // Utility to extract text from children
@@ -34,6 +38,10 @@ const CodeBlock = ({children, language = 'text', style, hideCopy = false}: Props
     hljs.configure({ classPrefix: 'token ' });
     hljs.registerLanguage("sql", sql);
     hljs.registerLanguage("json", json);
+    hljs.registerLanguage("yaml", yaml);
+    hljs.registerLanguage("bash", bash);
+    hljs.registerLanguage("ruby", ruby);
+    hljs.registerLanguage("python", python);
     hljs.highlightElement(ref.current);
   }, [ref]);
   return (
